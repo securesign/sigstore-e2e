37aad00235d3bae4e270eab585fceb60c9aa37d2
Elijah-Destigni <elijahdestigni73@gmail.com>
2026-02-12 16:29:04 +0000

    removed uneeded variables

--- tas-env-variables.ps1
+++ tas-env-variables.ps1
@@ -2,9 +2,7 @@
 $TUF_URL = $(oc get tuf -o jsonpath='{.items[0].status.url}')
 $OIDC_ROUTE = $(oc get route keycloak -n keycloak-system --template='{{.spec.host}}')
 $OIDC_ISSUER_URL = "https://$OIDC_ROUTE/auth/realms/trusted-artifact-signer"
-$FULCIO_URL = $(oc get fulcio -o jsonpath='{.items[0].status.url}')
-$REKOR_URL = $(oc get rekor -o jsonpath='{.items[0].status.url}')
-$REKOR_UI_URL = $(oc get rekor -o jsonpath='{.items[0].status.rekorSearchUIUrl}')
+
 
 if (-not $env:OIDC_CLIENT_ID) {
     $OIDC_CLIENT_ID = "trusted-artifact-signer"
@@ -14,34 +12,14 @@ if (-not $env:OIDC_CLIENT_ID) {
 
 # Export the environment variables for the current session
 $env:TUF_URL = $TUF_URL
-$env:OIDC_ISSUER_URL = $OIDC_ISSUER_URL
-$env:FULCIO_URL = $FULCIO_URL
-$env:REKOR_URL = $REKOR_URL
-$env:REKOR_UI_URL = $REKOR_UI_URL
-$env:REKOR_REKOR_SERVER = $REKOR_URL
-$env:SIGSTORE_REKOR_UI_URL = $REKOR_UI_URL
 $env:COSIGN_MIRROR = $TUF_URL
 $env:COSIGN_ROOT = "$TUF_URL/root.json"
 $env:COSIGN_OIDC_CLIENT_ID = $OIDC_CLIENT_ID
-$env:SIGSTORE_OIDC_ISSUER = $OIDC_ISSUER_URL
-$env:SIGSTORE_OIDC_CLIENT_ID = $OIDC_CLIENT_ID
-$env:COSIGN_YES = "true"
-$env:GITSIGN_FULCIO_URL = $FULCIO_URL
-$env:GITSIGN_REKOR_URL = $REKOR_URL
+
 
 # Print the environment variables to verify they are set
 Write-Output "TUF_URL=$env:TUF_URL"
-Write-Output "SIGSTORE_REKOR_UI_URL=$env:SIGSTORE_REKOR_UI_URL"
-Write-Output "REKOR_UI_URL=$env:REKOR_UI_URL"
-Write-Output "OIDC_ISSUER_URL=$env:OIDC_ISSUER_URL"
 Write-Output "COSIGN_MIRROR=$env:COSIGN_MIRROR"
 Write-Output "COSIGN_ROOT=$env:COSIGN_ROOT"
 Write-Output "COSIGN_OIDC_CLIENT_ID=$env:COSIGN_OIDC_CLIENT_ID"
-Write-Output "COSIGN_YES=$env:COSIGN_YES"
-Write-Output "SIGSTORE_OIDC_ISSUER=$env:SIGSTORE_OIDC_ISSUER"
-Write-Output "SIGSTORE_OIDC_CLIENT_ID=$env:SIGSTORE_OIDC_CLIENT_ID"
-Write-Output "FULCIO_URL=$env:FULCIO_URL"
-Write-Output "REKOR_URL=$env:REKOR_URL"
-Write-Output "REKOR_REKOR_SERVER=$env:REKOR_REKOR_SERVER"
-Write-Output "GITSIGN_FULCIO_URL=$env:GITSIGN_FULCIO_URL"
-Write-Output "GITSIGN_REKOR_URL=$env:GITSIGN_REKOR_URL"
+
--- tas-env-variables.sh
+++ tas-env-variables.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-if [ -z "$OIDC_ISSUER_URL" ]; then
-  export OIDC_ISSUER_URL=https://$(oc get route keycloak -n keycloak-system | tail -n 1 | awk '{print $2}')/auth/realms/trusted-artifact-signer
-fi
 
 if [ -z "$OIDC_CLIENT_ID" ]; then
   OIDC_CLIENT_ID="trusted-artifact-signer"
@@ -12,43 +9,14 @@ if [ -z "$TUF_URL" ]; then
   export TUF_URL=$(oc get tuf -o jsonpath='{.items[0].status.url}')
 fi
 
-if [ -z "$FULCIO_URL" ]; then
-  export FULCIO_URL=$(oc get fulcio -o jsonpath='{.items[0].status.url}')
-fi
-
-if [ -z "$REKOR_URL" ]; then
-  export REKOR_URL=$(oc get rekor -o jsonpath='{.items[0].status.url}')
-fi
-
-if [ -z "$REKOR_UI_URL" ]; then
-  export REKOR_UI_URL=$(oc get rekor -o jsonpath='{.items[0].status.rekorSearchUIUrl}')
-fi
 
 # Export the environment variables for the current session
-export REKOR_REKOR_SERVER=$REKOR_URL
-export SIGSTORE_REKOR_UI_URL=$REKOR_UI_URL
 export COSIGN_MIRROR=$TUF_URL
 export COSIGN_ROOT=$TUF_URL/root.json
 export COSIGN_OIDC_CLIENT_ID=$OIDC_CLIENT_ID
-export SIGSTORE_OIDC_ISSUER=$OIDC_ISSUER_URL
-export SIGSTORE_OIDC_CLIENT_ID=$OIDC_CLIENT_ID
-export COSIGN_YES="true"
-export GITSIGN_FULCIO_URL=$FULCIO_URL
-export GITSIGN_REKOR_URL=$REKOR_URL
 
 # Print the environment variables to verify they are set
 echo "TUF_URL=$TUF_URL"
-echo "SIGSTORE_REKOR_UI_URL=$SIGSTORE_REKOR_UI_URL"
-echo "REKOR_UI_URL=$REKOR_UI_URL"
-echo "OIDC_ISSUER_URL=$OIDC_ISSUER_URL"
 echo "COSIGN_MIRROR=$COSIGN_MIRROR"
 echo "COSIGN_ROOT=$COSIGN_ROOT"
 echo "COSIGN_OIDC_CLIENT_ID=$COSIGN_OIDC_CLIENT_ID"
-echo "COSIGN_YES=$COSIGN_YES"
-echo "SIGSTORE_OIDC_ISSUER=$SIGSTORE_OIDC_ISSUER"
-echo "SIGSTORE_OIDC_CLIENT_ID=$SIGSTORE_OIDC_CLIENT_ID"
-echo "FULCIO_URL=$FULCIO_URL"
-echo "REKOR_URL=$REKOR_URL"
-echo "REKOR_REKOR_SERVER=$REKOR_REKOR_SERVER"
-echo "GITSIGN_FULCIO_URL=$GITSIGN_FULCIO_URL"
-echo "GITSIGN_REKOR_URL=$GITSIGN_REKOR_URL"
